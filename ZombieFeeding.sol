a69bce89cad432df67a4ddfed1e01b5ce3e70a69
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-07-21 12:55:23 +0300

    Contract: Advanced; time, struct arg, function sec, modifier with args

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -24,10 +24,20 @@ contract ZombieFeeding is ZombieFactory {
         kittyContract = KittyInterface(_address);
     }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    function _triggerCooldown(Zombie storage _zombie) internal {
+        _zombie.readyTime = uint32(now + cooldownTime);
+    }
 
+    function _isReady(Zombie storage _zombie) internal view returns (bool) {
+        return (_zombie.readyTime <= now);
+    }
+
+    function _feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
+        require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+
+        require(_isReady(myZombie));
+
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
 
@@ -36,12 +46,13 @@ contract ZombieFeeding is ZombieFactory {
         }
 
         _createZombie("NoName", newDna);
+        _triggerCooldown(myZombie);
     }
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
         uint kittyDna;
         (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
 
-        feedAndMultiply(_zombieId, kittyDna, "kitty");
+        _feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
